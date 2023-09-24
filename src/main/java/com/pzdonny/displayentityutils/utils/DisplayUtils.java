a3666595c96c8a0b523f6183874f2f663bf2ee39
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-09-24 01:52:25 -0500

    Get correct model location and center of Interaction

--- src/main/java/com/pzdonny/displayentityutils/utils/DisplayUtils.java
+++ src/main/java/com/pzdonny/displayentityutils/utils/DisplayUtils.java
@@ -7,8 +7,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Display;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Interaction;
+import org.bukkit.entity.ItemDisplay;
 import org.bukkit.util.Transformation;
 import org.bukkit.util.Vector;
+import org.joml.Quaternionf;
 
 public final class DisplayUtils {
 
@@ -64,6 +67,7 @@ public final class DisplayUtils {
     }
 
 
+
     /**
      * Get the location of the model of a display entity. Not the entity's actual location but the location
      * based off of it's transformation
@@ -72,6 +76,39 @@ public final class DisplayUtils {
      */
     public static Location getModelLocation(Display display){
         Transformation transformation = display.getTransformation();
-        return display.getLocation().clone().add(Vector.fromJOML(transformation.getTranslation()));
+        Location modelLocation = display.getLocation().clone().add(Vector.fromJOML(transformation.getTranslation()));
+        if (display instanceof ItemDisplay){
+            modelLocation.subtract(0, Math.abs(display.getTransformation().getScale().y)/2, 0);
+        }
+        else{
+            Quaternionf leftRotation = transformation.getLeftRotation();
+            float x = leftRotation.x;
+            float y = leftRotation.y;
+            float z = leftRotation.z;
+            Vector v = new Vector(-0.5, 0, 0.5);
+
+
+            v.rotateAroundX(x);
+            v.rotateAroundY(y);
+            v.rotateAroundX(z);
+            if (x % Math.PI == 0){
+                v.setX(0.5);
+            }
+            modelLocation.add(v);
+        }
+        return modelLocation;
     }
+
+    /**
+     * Gets the center location of an Interaction entity
+     * @param interaction The interaction entity get the center of
+     * @return The interaction's center location
+     */
+    public static Location getInteractionCenter(Interaction interaction){
+        Location loc = interaction.getLocation().clone();
+        double yCenter = interaction.getInteractionHeight()/2;
+        loc.add(0, yCenter, 0);
+        return loc;
+    }
+
 }
