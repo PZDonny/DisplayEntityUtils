3f44de3c5efb74f48b4ed2ef419485ff35122ff0
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-16 04:05:49 -0500

    No Changes

--- src/main/java/com/pzdonny/displayentityutils/utils/DisplayEntities/DisplayEntityGroup.java
+++ src/main/java/com/pzdonny/displayentityutils/utils/DisplayEntities/DisplayEntityGroup.java
@@ -20,8 +20,6 @@ public class DisplayEntityGroup implements Serializable{
     DisplayEntity masterEntity;
     private String tag;
 
-
-
     @Serial
     private static final long serialVersionUID = 99L;
 
