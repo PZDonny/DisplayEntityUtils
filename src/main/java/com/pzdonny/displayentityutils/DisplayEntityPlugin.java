fb222d988210517fc4656294b94150feef3b7ed2
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-02-09 18:02:48 -0600

    No Changes

--- src/main/java/com/pzdonny/displayentityutils/DisplayEntityPlugin.java
+++ src/main/java/com/pzdonny/displayentityutils/DisplayEntityPlugin.java
@@ -14,7 +14,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
