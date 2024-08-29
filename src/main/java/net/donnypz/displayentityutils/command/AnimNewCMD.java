8b74ea4cd8ff79717e2f3e85a1e7ebcf661c0687
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-08-29 03:51:29 -0500

    No Changes

--- src/main/java/net/donnypz/displayentityutils/command/AnimNewCMD.java
+++ src/main/java/net/donnypz/displayentityutils/command/AnimNewCMD.java
@@ -3,6 +3,8 @@ package net.donnypz.displayentityutils.command;
 import net.donnypz.displayentityutils.DisplayEntityPlugin;
 import net.donnypz.displayentityutils.managers.DisplayAnimationManager;
 import net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayAnimation;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -12,16 +14,20 @@ class AnimNewCMD implements SubCommand{
         if (!DisplayEntityPluginCommand.hasPermission(player, Permission.ANIM_NEW)){
             return;
         }
-
+        SpawnedDisplayAnimation animation;
         if (args.length < 3) {
-            DisplayAnimationManager.setSelectedSpawnedAnimation(player, new SpawnedDisplayAnimation());
-        } else {
-            DisplayAnimationManager.setSelectedSpawnedAnimation(player, new SpawnedDisplayAnimation(args[2]));
+            animation = new SpawnedDisplayAnimation();
+        }
+        else{
+            animation = new SpawnedDisplayAnimation(args[2]);
         }
 
+        DisplayAnimationManager.setSelectedSpawnedAnimation(player, animation);
         player.sendMessage(DisplayEntityPlugin.pluginPrefix + ChatColor.GREEN + "You can now prepare a new animation");
-        player.sendMessage(ChatColor.GRAY + " - Before animating, if you want your display entity group to retain it's pose, you must save it's current pose " +
-                "as a single frame animation");
-        player.sendMessage(ChatColor.YELLOW + " - To create an animation only affecting part tags do \"/mdis anim new <part-tag>\"");
+        player.sendMessage(Component.text(" - Before animating, if you want your display entity group to retain it's pose, you must save it's current pose " +
+                "as a single frame animation", NamedTextColor.GRAY));
+        if (!animation.isPartAnimation()){
+            player.sendMessage(Component.text(" - To create an animation only manipulating certain parts do \"/mdis anim new [part-tag]\"", NamedTextColor.YELLOW));
+        }
     }
 }
