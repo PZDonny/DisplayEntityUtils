61843a71d3fc1309a74875ca2f105ac658d6fd1e
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-08-29 03:45:41 -0500

    No Changes

--- src/main/java/net/donnypz/displayentityutils/listeners/player/DEUPlayerConnectionListener.java
+++ src/main/java/net/donnypz/displayentityutils/listeners/player/DEUPlayerConnectionListener.java
@@ -5,8 +5,10 @@ import net.donnypz.displayentityutils.managers.DisplayGroupManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.jetbrains.annotations.ApiStatus;
 
-public class DEUPlayerConnectionListener implements Listener {
+@ApiStatus.Internal
+public final class DEUPlayerConnectionListener implements Listener {
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e){
