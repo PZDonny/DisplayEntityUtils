4c6194e8294d8c61983a58f8ccf8da53d33f0720
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-08-29 03:49:52 -0500

    Part Tag / PDC Rework

--- src/main/java/net/donnypz/displayentityutils/utils/InteractionCommand.java
+++ src/main/java/net/donnypz/displayentityutils/utils/InteractionCommand.java
@@ -1,15 +1,19 @@
 package net.donnypz.displayentityutils.utils;
 
+import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.ApiStatus;
+
 public class InteractionCommand {
     String command;
     boolean isLeftClick;
     boolean isConsoleCommand;
+    NamespacedKey key;
 
-
-    InteractionCommand(String command, boolean isLeftClick, boolean isConsoleCommand){
+    InteractionCommand(String command, boolean isLeftClick, boolean isConsoleCommand, NamespacedKey key){
         this.command = command;
         this.isLeftClick = isLeftClick;
         this.isConsoleCommand = isConsoleCommand;
+        this.key = key;
     }
 
 
@@ -23,5 +27,10 @@ public class InteractionCommand {
 
     public boolean isConsoleCommand() {
         return isConsoleCommand;
+    }
+
+    @ApiStatus.Internal
+    public NamespacedKey getKey(){
+        return key;
     }
 }
