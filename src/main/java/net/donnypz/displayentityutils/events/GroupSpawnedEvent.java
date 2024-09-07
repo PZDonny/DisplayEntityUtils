174ba231540a54f757152eb8e89b2338ce899482
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-09-06 21:32:53 -0500

    Add getter method for SpawnReason, Update javadocs

--- src/main/java/net/donnypz/displayentityutils/events/GroupSpawnedEvent.java
+++ src/main/java/net/donnypz/displayentityutils/events/GroupSpawnedEvent.java
@@ -3,22 +3,36 @@ package net.donnypz.displayentityutils.events;
 import net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityGroup;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 /**
- * Called when a SpawnDisplayEntityGroup is created.
+ * Called when a SpawnDisplayEntityGroup is spawned from a {@link net.donnypz.displayentityutils.utils.DisplayEntities.DisplayEntityGroup}.
  */
 public class GroupSpawnedEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     SpawnedDisplayEntityGroup spawnedDisplayEntityGroup;
+    SpawnReason spawnReason;
 
-    public GroupSpawnedEvent(SpawnedDisplayEntityGroup group, SpawnReason spawnReason){
+    public GroupSpawnedEvent(@NotNull SpawnedDisplayEntityGroup group, @NotNull SpawnReason spawnReason){
         this.spawnedDisplayEntityGroup = group;
+        this.spawnReason = spawnReason;
     }
 
-    public SpawnedDisplayEntityGroup getGroup() {
+    /**
+     * Get the {@link SpawnedDisplayEntityGroup} involved in this event.
+     * @return a {@link SpawnedDisplayEntityGroup}
+     */
+    public @NotNull SpawnedDisplayEntityGroup getGroup() {
         return spawnedDisplayEntityGroup;
     }
 
+    /**
+     * Get the {@link SpawnReason} for this event
+     * @return a spawn reason
+     */
+    public SpawnReason getSpawnReason() {
+        return spawnReason;
+    }
 
     @Override
     public HandlerList getHandlers() {
