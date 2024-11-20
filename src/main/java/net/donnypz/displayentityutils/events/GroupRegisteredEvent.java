3cb73a3adf869e28308c6bfe92db10cec26c469d
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-11-19 21:06:46 -0600

    Add GroupRegisteredEvent for nearby group detected

---
+++ src/main/java/net/donnypz/displayentityutils/events/GroupRegisteredEvent.java
@@ -0,0 +1,37 @@
+package net.donnypz.displayentityutils.events;
+
+import net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityGroup;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Called when a {@link SpawnedDisplayEntityGroup} is registered by nearby selection
+ */
+public class GroupRegisteredEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+    SpawnedDisplayEntityGroup spawnedDisplayEntityGroup;
+
+    public GroupRegisteredEvent(SpawnedDisplayEntityGroup group){
+        this.spawnedDisplayEntityGroup = group;
+    }
+
+    /**
+     * Get the {@link SpawnedDisplayEntityGroup} involved in this event
+     * @return a group
+     */
+    public SpawnedDisplayEntityGroup getGroup() {
+        return spawnedDisplayEntityGroup;
+    }
+
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
