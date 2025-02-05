5abc440fe64466e6559176f6ea8f3e672f4716cf
Jay <76460079+DonnyPZ@users.noreply.github.com>
2025-02-04 20:30:11 -0600

    No Changes

--- src/main/java/net/donnypz/displayentityutils/utils/CullOption.java
+++ src/main/java/net/donnypz/displayentityutils/utils/CullOption.java
@@ -1,20 +1,22 @@
 package net.donnypz.displayentityutils.utils;
 
+import net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityGroup;
+
 public enum CullOption {
     /**
-     * This setting applies the culling size of the largest part of a {@link net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityGroup}
+     * This setting applies the culling size of the largest part of a {@link SpawnedDisplayEntityGroup}
      * to all parts within a group.
      */
     LARGEST,
 
     /**
-     * This setting applies the culling size of all parts of a {@link net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityGroup}
+     * This setting applies the culling size of all parts of a {@link SpawnedDisplayEntityGroup}
      * based on their individual transformation scaling
      */
     LOCAL,
 
     /**
-     * This setting removes culling from all parts within a {@link net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityGroup}
+     * This setting removes culling from all parts within a {@link SpawnedDisplayEntityGroup}
      */
     NONE
 }
