7db0818681d3724fb95f6efe65a3e04e8ee55dc9
Jay <76460079+DonnyPZ@users.noreply.github.com>
2025-03-03 23:34:21 -0600

    Add documentation

--- src/main/java/net/donnypz/displayentityutils/utils/FollowType.java
+++ src/main/java/net/donnypz/displayentityutils/utils/FollowType.java
@@ -1,8 +1,22 @@
 package net.donnypz.displayentityutils.utils;
 
+import org.bukkit.entity.LivingEntity;
+
 public enum FollowType {
+    /**
+     * Follow an entity's looking direction based on its pitch and yaw
+     */
     PITCH_AND_YAW,
+    /**
+     * Follow an entity's looking direction based on its pitch
+     */
     PITCH,
+    /**
+     * Follow an entity's looking direction based on its yaw
+     */
     YAW,
+    /**
+     * Follow a {@link LivingEntity}'s body yaw
+     */
     BODY,
 }
