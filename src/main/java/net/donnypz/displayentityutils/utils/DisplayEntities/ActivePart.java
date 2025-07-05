be30bfb6b153737b01c22bde64be01cd3a8f479f
Jay <76460079+DonnyPZ@users.noreply.github.com>
2025-07-05 18:37:30 -0500

    Move PartData into ActivePart

--- src/main/java/net/donnypz/displayentityutils/utils/DisplayEntities/ActivePart.java
+++ src/main/java/net/donnypz/displayentityutils/utils/DisplayEntities/ActivePart.java
@@ -1,7 +1,10 @@
 package net.donnypz.displayentityutils.utils.DisplayEntities;
 
+import org.bukkit.entity.Entity;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public abstract class ActivePart {
@@ -25,4 +28,58 @@ public abstract class ActivePart {
         return type;
     }
 
+    static class PartData {
+
+        private final UUID entityUUID;
+        private String worldName;
+
+        PartData(@NotNull Entity entity) {
+            this(entity.getUniqueId(), entity.getWorld().getName());
+        }
+
+        PartData(@NotNull UUID entityUUID, @NotNull String worldName) {
+            this.entityUUID = entityUUID;
+            this.worldName = worldName;
+        }
+
+        void setWorldName(String worldName) {
+            this.worldName = worldName;
+        }
+
+        /**
+         * Get the UUID of the entity this PartData represents
+         *
+         * @return a UUID
+         */
+        public UUID getUUID() {
+            return entityUUID;
+        }
+
+        /**
+         * Get the world name of the entity this PartData represents
+         *
+         * @return a string
+         */
+        public String getWorldName() {
+            return worldName;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            PartData data = (PartData) obj;
+            return entityUUID.equals(data.entityUUID) && worldName.equals(data.worldName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(entityUUID, worldName);
+        }
+    }
 }
