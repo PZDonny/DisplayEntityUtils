b4174bbaec6f43a967ac299749ae9b3d41c096e0
Jay <76460079+DonnyPZ@users.noreply.github.com>
2025-07-09 21:47:49 -0500

    Add packet based interaction translations and scaling

--- src/main/java/net/donnypz/displayentityutils/utils/PacketUtils.java
+++ src/main/java/net/donnypz/displayentityutils/utils/PacketUtils.java
@@ -5,6 +5,7 @@ import com.github.retrooper.packetevents.wrapper.play.server.*;
 import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import net.donnypz.displayentityutils.DisplayEntityPlugin;
 import net.donnypz.displayentityutils.managers.DEUUser;
+import net.donnypz.displayentityutils.utils.DisplayEntities.ActivePart;
 import net.donnypz.displayentityutils.utils.DisplayEntities.PacketDisplayEntityPart;
 import net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayEntityPart;
 import net.donnypz.displayentityutils.utils.packet.DisplayAttributeMap;
@@ -20,6 +21,7 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.SequencedCollection;
 
 public final class PacketUtils {
@@ -93,6 +95,14 @@ public final class PacketUtils {
         }
     }
 
+    /**
+     * Change the scale of an interaction entity for all players who can visibly see the given {@link PacketDisplayEntityPart}
+     * @param part the part
+     * @param newHeight the new height
+     * @param newWidth the new width
+     * @param durationInTicks the duration in ticks
+     * @param delayInTicks the delay in ticks
+     */
     public static void scaleInteraction(@NotNull PacketDisplayEntityPart part, float newHeight, float newWidth, int durationInTicks, int delayInTicks){
         if (part.getType() != SpawnedDisplayEntityPart.PartType.INTERACTION) return;
         if (durationInTicks <= 0 && delayInTicks <= 0){
@@ -117,11 +127,115 @@ public final class PacketUtils {
         }.runTaskTimerAsynchronously(DisplayEntityPlugin.getInstance(), delayInTicks, 1);
     }
 
-    static void sendInteractionPacket(PacketDisplayEntityPart part, float newHeight, float newWidth){
+
+    /**
+     * Scale an interaction entity over time, displayed only for the given player
+     * @param player the player
+     * @param interaction the interaction
+     * @param newHeight the height the interaction will be scaled to
+     * @param newWidth the width the interaction will be scale to
+     * @param durationInTicks how long the scaling should take
+     * @param delayInTicks how long before the scaling should begin
+     */
+    public static void scaleInteraction(@NotNull Player player, @NotNull Interaction interaction, float newHeight, float newWidth, int durationInTicks, int delayInTicks){
+        scaleInteraction(List.of(player), interaction, newHeight, newWidth, durationInTicks, delayInTicks);
+    }
+
+    /**
+     * Scale an interaction entity over time, displayed only for the given players
+     * @param players the players
+     * @param interaction the interaction
+     * @param newHeight the height the interaction will be scaled to
+     * @param newWidth the width the interaction will be scale to
+     * @param durationInTicks how long the scaling should take
+     * @param delayInTicks how long before the scaling should begin
+     */
+    public static void scaleInteraction(@NotNull Collection<Player> players, @NotNull Interaction interaction, float newHeight, float newWidth, int durationInTicks, int delayInTicks){
+        float height = interaction.getInteractionHeight();
+        float width = interaction.getInteractionWidth();
+        scaleInteraction(players, interaction.getEntityId(), height, width, newHeight, newWidth, durationInTicks, delayInTicks);
+    }
+
+    /**
+     * Scale an interaction entity over time, displayed only for the given player
+     * @param player the player
+     * @param part the interaction part
+     * @param newHeight the height the interaction will be scaled to
+     * @param newWidth the width the interaction will be scale to
+     * @param durationInTicks how long the scaling should take
+     * @param delayInTicks how long before the scaling should begin
+     */
+    public static void scaleInteraction(@NotNull Player player, @NotNull ActivePart part, float newHeight, float newWidth, int durationInTicks, int delayInTicks){
+        scaleInteraction(List.of(player), part, newHeight, newWidth, durationInTicks, delayInTicks);
+    }
+
+    /**
+     * Scale an interaction entity over time, displayed only for the given players
+     * @param players the players
+     * @param part the interaction part
+     * @param newHeight the height the interaction will be scaled to
+     * @param newWidth the width the interaction will be scale to
+     * @param durationInTicks how long the scaling should take
+     * @param delayInTicks how long before the scaling should begin
+     */
+    public static void scaleInteraction(@NotNull Collection<Player> players, @NotNull ActivePart part, float newHeight, float newWidth, int durationInTicks, int delayInTicks){
+        if (part.getType() != SpawnedDisplayEntityPart.PartType.INTERACTION) return;
+        float height = part.getInteractionHeight();
+        float width = part.getInteractionWidth();
+        scaleInteraction(players, part.getEntityId(), height, width, newHeight, newWidth, durationInTicks, delayInTicks);
+    }
+
+    private static void scaleInteraction(Collection<Player> players, int entityId, float height, float width, float newHeight, float newWidth, int durationInTicks, int delayInTicks){
+        if (durationInTicks <= 0 && delayInTicks <= 0){
+            sendInteractionPacket(entityId, newHeight, newWidth, players);
+            return;
+        }
+
+        float heightChange = (height-newHeight)/durationInTicks;
+        float widthChange = (width-newWidth)/durationInTicks;
+        new BukkitRunnable(){
+            int timeRan = 0;
+            @Override
+            public void run() {
+                if (timeRan == durationInTicks){
+                    sendInteractionPacket(entityId, newHeight, newWidth, players);
+                    cancel();
+                    return;
+                }
+                sendInteractionPacket(entityId, height-heightChange, width-widthChange, players);
+                timeRan++;
+            }
+        }.runTaskTimerAsynchronously(DisplayEntityPlugin.getInstance(), delayInTicks, 1);
+    }
+
+    private static void sendInteractionPacket(PacketDisplayEntityPart part, float newHeight, float newWidth){
         part.setAttributes(new DisplayAttributeMap()
                 .add(DisplayAttributes.Interaction.HEIGHT, newHeight)
                 .add(DisplayAttributes.Interaction.WIDTH, newWidth));
     }
+
+    private static void sendInteractionPacket(int entityId, float newHeight, float newWidth, Collection<Player> players){
+        new PacketAttributeContainer()
+                .setAttribute(DisplayAttributes.Interaction.HEIGHT, newHeight)
+                .setAttribute(DisplayAttributes.Interaction.WIDTH, newWidth)
+                .sendAttributesUsingPlayers(players, entityId);
+    }
+
+
+    /**
+     * Attempts to change the translation of a packet-based interaction entity similar
+     * to a Display Entity, through smooth teleportation.
+     * Doing multiple translations on an Interaction entity at the same time may have unexpected results
+     * @param part the interaction part
+     * @param direction The direction to translate the interaction entity
+     * @param distance How far the interaction entity should be translated
+     * @param durationInTicks How long it should take for the translation to complete
+     * @param delayInTicks How long before the translation should begin
+     */
+    public static void translateInteraction(@NotNull PacketDisplayEntityPart part, @NotNull Direction direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(part, direction.getVector(part), distance, durationInTicks, delayInTicks);
+    }
+
 
     /**
      * Attempts to change the translation of a packet-based interaction entity similar
@@ -172,22 +286,97 @@ public final class PacketUtils {
         }.runTaskTimerAsynchronously(DisplayEntityPlugin.getInstance(), delayInTicks, 1);
     }
 
-
-    /**
-     * Attempts to change the translation of a packet-based interaction entity similar
-     * to a Display Entity, through smooth teleportation.
-     * Doing multiple translations on an Interaction entity at the same time may have unexpected results
-     * @param part the interaction part
-     * @param direction The direction to translate the interaction entity
-     * @param distance How far the interaction entity should be translated
-     * @param durationInTicks How long it should take for the translation to complete
-     * @param delayInTicks How long before the translation should begin
-     */
-    public static void translateInteraction(@NotNull PacketDisplayEntityPart part, @NotNull Direction direction, double distance, int durationInTicks, int delayInTicks){
-        translateInteraction(part, direction.getVector(part), distance, durationInTicks, delayInTicks);
+    public static void translateInteraction(@NotNull Player player, @NotNull Interaction interaction, @NotNull Direction direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(player, interaction, direction.getVector(interaction), distance, durationInTicks, delayInTicks);
     }
 
+    public static void translateInteraction(@NotNull Player player, @NotNull Interaction interaction, @NotNull Vector direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(List.of(player), interaction, direction, distance, durationInTicks, delayInTicks);
+    }
 
+    public static void translateInteraction(@NotNull Player player, @NotNull ActivePart part, @NotNull Direction direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(player, part, direction.getVector(getLocation(part)), distance, durationInTicks, delayInTicks);
+    }
+
+    public static void translateInteraction(@NotNull Player player, @NotNull ActivePart part, @NotNull Vector direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(List.of(player), part, direction, distance, durationInTicks, delayInTicks);
+    }
+
+    public static void translateInteraction(@NotNull Collection<Player> players, @NotNull Interaction interaction, @NotNull Direction direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(players, interaction, direction.getVector(interaction), distance, durationInTicks, delayInTicks);
+    }
+
+    public static void translateInteraction(@NotNull Collection<Player> players, @NotNull Interaction interaction, @NotNull Vector direction, double distance, int durationInTicks, int delayInTicks){
+        Location l = interaction.getLocation();
+        translateInteraction(players, interaction.getEntityId(), l, l.getYaw(), direction, distance, durationInTicks, delayInTicks);
+    }
+
+    public static void translateInteraction(@NotNull Collection<Player> players, @NotNull ActivePart part, @NotNull Direction direction, double distance, int durationInTicks, int delayInTicks){
+        translateInteraction(players, part, direction.getVector(getLocation(part)), distance, durationInTicks, delayInTicks);
+    }
+
+    public static void translateInteraction(@NotNull Collection<Player> players, @NotNull ActivePart part, @NotNull Vector direction, double distance, int durationInTicks, int delayInTicks){
+        if (part.getType() != SpawnedDisplayEntityPart.PartType.INTERACTION) return;
+        Location l = getLocation(part);
+        if (l == null) return;
+        translateInteraction(players, part.getEntityId(), l, l.getYaw(), direction, distance, durationInTicks, delayInTicks);
+    }
+
+    private static void translateInteraction(Collection<Player> players, int entityId, Location location, float lastYaw, Vector direction, double distance, int durationInTicks, int delayInTicks){
+        Location destination = location.clone().add(direction.clone().normalize().multiply(distance));
+        if (durationInTicks <= 0 && delayInTicks <= 0){
+            sendInteractionTeleportPacket(entityId, destination, players);
+            return;
+        }
+
+        double movementIncrement = distance/Math.max(durationInTicks, 1);
+        Vector incrementVector = direction
+                .clone()
+                .normalize()
+                .multiply(movementIncrement);
+
+        new BukkitRunnable(){
+            double currentDistance = 0;
+            float finalLastYaw = lastYaw;
+            Location tpLoc = location.clone();
+            @Override
+            public void run() {
+                float newYaw = tpLoc.getYaw();
+                if (newYaw != finalLastYaw){
+                    incrementVector.rotateAroundY(Math.toRadians(finalLastYaw-newYaw));
+                    finalLastYaw = newYaw;
+                }
+                currentDistance+=Math.abs(movementIncrement);
+                tpLoc.add(incrementVector);
+
+                if (currentDistance >= distance){
+                    sendInteractionTeleportPacket(entityId, destination, players);
+                    cancel();
+                }
+                else{
+                    sendInteractionTeleportPacket(entityId, tpLoc, players);
+                }
+            }
+        }.runTaskTimerAsynchronously(DisplayEntityPlugin.getInstance(), delayInTicks, 1);
+    }
+
+    private static void sendInteractionTeleportPacket(int entityId, Location location, Collection<Player> players){
+        for (Player p : players){
+            PacketEvents.getAPI().getPlayerManager().sendPacket(p, new WrapperPlayServerEntityTeleport(entityId,
+                    SpigotConversionUtil.fromBukkitLocation(location),
+                    false));
+        }
+    }
+
+    private static Location getLocation(ActivePart part){
+        if (part instanceof SpawnedDisplayEntityPart sp){
+            return sp.getLocation();
+        }
+        else if (part instanceof PacketDisplayEntityPart pp){
+            return pp.getLocation();
+        }
+        return null;
+    }
 
     public static void destroyEntity(@NotNull Player player, @NotNull PacketDisplayEntityPart part){
         if (!part.isTrackedBy(player)) {
