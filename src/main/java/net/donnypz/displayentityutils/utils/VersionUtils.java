406e52c888be9b35a9d2f7f1f1d676c70410ed95
Jay <76460079+DonnyPZ@users.noreply.github.com>
2025-07-24 19:33:02 -0500

    Reduce redundant version checks

--- src/main/java/net/donnypz/displayentityutils/utils/VersionUtils.java
+++ src/main/java/net/donnypz/displayentityutils/utils/VersionUtils.java
@@ -14,26 +14,24 @@ public final class VersionUtils {
     public static boolean IS_1_21_7 = Bukkit.getUnsafe().getProtocolVersion() >= 772;
 
     public static boolean canViewDialogs(Player player, boolean sendErrorMessage){
-        if (!serverHasDialogs()){
+        if (player.getProtocolVersion() < 771){ //1.21.6
+            if (sendErrorMessage){
+                player.sendMessage(DisplayEntityPlugin.pluginPrefix
+                        .append(Component.text("You can only view Dialog Menus when playing on version 1.21.6 or higher!", NamedTextColor.RED)));
+            }
+            return false;
+        }
+        else if (!serverHasDialogs()){
             if (sendErrorMessage){
                 player.sendMessage(DisplayEntityPlugin.pluginPrefix
                         .append(Component.text("This server's version is below 1.21.7 and cannot use Dialog Menus!", NamedTextColor.RED)));
             }
             return false;
         }
-        else if (player.getProtocolVersion() < 771){ //1.21.6
-            if (!serverHasDialogs()){
-                if (sendErrorMessage){
-                    player.sendMessage(DisplayEntityPlugin.pluginPrefix
-                            .append(Component.text("You can only view Dialog Menus when playing on version 1.21.6 or higher!", NamedTextColor.RED)));
-                }
-            }
-            return false;
-        }
         return true;
     }
 
-    public static boolean serverHasDialogs(){ //Dialog API came to paper in 1.21.7, Dialog System came to MC in 1.21.6
+    public static boolean serverHasDialogs(){ //Dialog API came to Paper in 1.21.7, Dialog System came to MC in 1.21.6
         return IS_1_21_7;
     }
 }
