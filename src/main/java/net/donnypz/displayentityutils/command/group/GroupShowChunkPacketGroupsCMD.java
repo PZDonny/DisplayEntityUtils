627c309f408be46c341ab609cf5b424c519c3edb
Jay <76460079+DonnyPZ@users.noreply.github.com>
2025-08-01 05:27:34 -0500

    Added "/mdis group hidepacketgroups" and "/mdis group showpacketgroups"

---
+++ src/main/java/net/donnypz/displayentityutils/command/group/GroupShowChunkPacketGroupsCMD.java
@@ -0,0 +1,28 @@
+package net.donnypz.displayentityutils.command.group;
+
+import net.donnypz.displayentityutils.DisplayEntityPlugin;
+import net.donnypz.displayentityutils.command.DEUSubCommand;
+import net.donnypz.displayentityutils.command.Permission;
+import net.donnypz.displayentityutils.command.PlayerSubCommand;
+import net.donnypz.displayentityutils.events.GroupSpawnedEvent;
+import net.donnypz.displayentityutils.utils.DisplayEntities.PacketDisplayEntityGroup;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+class GroupShowChunkPacketGroupsCMD extends PlayerSubCommand {
+    GroupShowChunkPacketGroupsCMD(@NotNull DEUSubCommand parentSubCommand) {
+        super("showpacketgroups", parentSubCommand, Permission.GROUP_CHUNK_PACKET_GROUP_VISIBILITY);
+    }
+
+    @Override
+    public void execute(Player player, String[] args) {
+        for (PacketDisplayEntityGroup pg : PacketDisplayEntityGroup.getGroups(player.getChunk())){
+            if (pg.getChunkPacketGroupId() != -1){
+                pg.showToPlayer(player, GroupSpawnedEvent.SpawnReason.COMMAND);
+            }
+        }
+        player.sendMessage(DisplayEntityPlugin.pluginPrefix.append(Component.text("Showing all chunk-stored, packet-based groups in this chunk.", NamedTextColor.GREEN)));
+    }
+}
