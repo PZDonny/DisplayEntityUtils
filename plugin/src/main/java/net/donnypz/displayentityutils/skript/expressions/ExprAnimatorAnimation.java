b5f1df2f5c9c93efc103c31e18a26d05c2bdd661
Jay <76460079+PZDonny@users.noreply.github.com>
2025-09-08 21:56:19 -0500

    Add Skript syntax to get an animator's animation

---
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/expressions/ExprAnimatorAnimation.java
@@ -0,0 +1,42 @@
+package net.donnypz.displayentityutils.skript.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import net.donnypz.displayentityutils.utils.DisplayEntities.DisplayAnimator;
+import net.donnypz.displayentityutils.utils.DisplayEntities.SpawnedDisplayAnimation;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Spawned Animation of Display Animator")
+@Description("Get the animation set for a display animator.")
+@Examples({"set {_animation} to {_displayanimator}'s animation"})
+@Since("3.3.1")
+public class ExprAnimatorAnimation extends SimplePropertyExpression<DisplayAnimator, SpawnedDisplayAnimation> {
+
+    static {
+        register(ExprAnimatorAnimation.class, SpawnedDisplayAnimation.class, "[deu] [spawned] animation", "displayanimator");
+    }
+
+    @Override
+    public Class<? extends SpawnedDisplayAnimation> getReturnType() {
+        return SpawnedDisplayAnimation.class;
+    }
+
+    @Override
+    @Nullable
+    public SpawnedDisplayAnimation convert(DisplayAnimator animator) {
+        return animator != null ? animator.getAnimation() : null;
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "animation";
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+}
