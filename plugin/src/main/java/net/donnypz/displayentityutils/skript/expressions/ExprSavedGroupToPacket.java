09e79e64fea9ad4294e84fcc8f8eb4f220731ba5
Jay <76460079+PZDonny@users.noreply.github.com>
2025-10-12 01:15:30 -0500

    Update syntax to spawn a persistent packet based group

--- plugin/src/main/java/net/donnypz/displayentityutils/skript/expressions/ExprSavedGroupToPacket.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/expressions/ExprSavedGroupToPacket.java
@@ -10,6 +10,7 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import net.donnypz.displayentityutils.managers.DisplayGroupManager;
 import net.donnypz.displayentityutils.utils.DisplayEntities.DisplayEntityGroup;
 import net.donnypz.displayentityutils.utils.DisplayEntities.PacketDisplayEntityGroup;
 import org.bukkit.Location;
@@ -18,16 +19,20 @@ import org.jetbrains.annotations.Nullable;
 
 @Name("Saved Group to Packet Group")
 @Description("Create a packet based group from a saved group")
-@Examples({"set {_packetgroup} to packet based {_savedgroup} spawned at {_location}"})
-@Since("3.0.0")
+@Examples({"set {_packetgroup} to packet based {_savedgroup} spawned at {_location}",
+        "",
+        "#(3.3.4+) Spawn packet based group that will persist after restarts",
+        "set {_packetgroup} to persistent packet based {_savedgroup} spawned at {_location}"})
+@Since("3.0.0, 3.3.4 (Persistent)")
 public class ExprSavedGroupToPacket extends SimpleExpression<PacketDisplayEntityGroup> {
 
     static{
-        Skript.registerExpression(ExprSavedGroupToPacket.class, PacketDisplayEntityGroup.class, ExpressionType.COMBINED, "packet [based] %savedgroup% spawned at %location%");
+        Skript.registerExpression(ExprSavedGroupToPacket.class, PacketDisplayEntityGroup.class, ExpressionType.COMBINED, "[:persistent] packet [based] %savedgroup% spawned at %location%");
     }
 
     private Expression<DisplayEntityGroup> savedGroup;
     private Expression<Location> location;
+    boolean persistent;
 
     @Override
     protected PacketDisplayEntityGroup @Nullable [] get(Event event) {
@@ -35,7 +40,12 @@ public class ExprSavedGroupToPacket extends SimpleExpression<PacketDisplayEntity
         if (saved == null) return new PacketDisplayEntityGroup[0];
         Location loc = location.getSingle(event);
         if (loc == null) return new PacketDisplayEntityGroup[0];
-        return new PacketDisplayEntityGroup[]{saved.createPacketGroup(loc, true)};
+        if (persistent){
+            return new PacketDisplayEntityGroup[]{DisplayGroupManager.addPersistentPacketGroup(loc, saved, false)};
+        }
+        else{
+            return new PacketDisplayEntityGroup[]{saved.createPacketGroup(loc, true)};
+        }
     }
 
     @Override
@@ -50,13 +60,14 @@ public class ExprSavedGroupToPacket extends SimpleExpression<PacketDisplayEntity
 
     @Override
     public String toString(@Nullable Event event, boolean debug) {
-        return "packet based " + savedGroup.toString(event, debug)+" spawned at " + location.toString(event, debug);
+        return (persistent ? "persistent" : "")+"packet based " + savedGroup.toString(event, debug)+" spawned at " + location.toString(event, debug);
     }
 
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         savedGroup = (Expression<DisplayEntityGroup>) expressions[0];
         location = (Expression<Location>) expressions[1];
+        persistent = parseResult.hasTag("persistent");
         return true;
     }
 }
