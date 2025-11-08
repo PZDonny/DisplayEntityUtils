4bb6bade52342a919d346535e63425d7a279a5a1
Jay <76460079+PZDonny@users.noreply.github.com>
2025-11-08 02:56:45 -0600

    Fix`Active Part Data not getting the text of a text display part, with color, allow set for multiple parts

--- plugin/src/main/java/net/donnypz/displayentityutils/skript/expressions/ExprActivePartData.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/expressions/ExprActivePartData.java
@@ -9,6 +9,7 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.util.Utils;
 import ch.njol.skript.util.chat.BungeeConverter;
 import ch.njol.skript.util.chat.ChatMessages;
 import ch.njol.util.Kleenean;
@@ -16,12 +17,15 @@ import ch.njol.util.coll.CollectionUtils;
 import net.donnypz.displayentityutils.utils.DisplayEntities.ActivePart;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
 
 @Name("Active Part Data")
 @Description("Get the data of an active part based on its part type.")
@@ -30,12 +34,14 @@ import org.jetbrains.annotations.Nullable;
             "set {_text} to {_packetpart}'s deu text",
             "",
             "#Change Data",
+            "set {_parts::*}'s deu text to \"&aMy New Text\"",
+            "",
             "if {_spawnedpart}'s active part type is block_display:",
             "\tset {_spawnedpart}'s deu block to oak_stairs[facing=east]"})
 @Since("3.3.6")
 public class ExprActivePartData extends SimplePropertyExpression<ActivePart, Object> {
     static {
-        register(ExprActivePartData.class, Object.class, "deu (1¦block|2¦item|3¦text|4¦[interaction] width|5¦[interaction] height)", "activepart");
+        register(ExprActivePartData.class, Object.class, "deu (1¦block|2¦item|3¦text|4¦[interaction] width|5¦[interaction] height)", "activeparts");
     }
 
     int mark;
@@ -55,14 +61,27 @@ public class ExprActivePartData extends SimplePropertyExpression<ActivePart, Obj
     @Override
     @Nullable
     public Object convert(ActivePart part) {
-        return switch (mark) {
-            case 1 -> part.getBlockDisplayBlock();
-            case 2 -> part.getItemDisplayItem();
-            case 3 -> part.getTextDisplayText();
-            case 4 -> part.getInteractionWidth();
-            case 5 -> part.getInteractionHeight();
-            default -> null;
-        };
+        switch (mark) {
+            case 1 -> {
+                return part.getBlockDisplayBlock();
+            }
+            case 2 -> {
+                return part.getItemDisplayItem();
+            }
+            case 3 -> {
+                Component comp = part.getTextDisplayText();
+                return Utils.replaceChatStyles(LegacyComponentSerializer.legacySection().serialize(comp));
+            }
+            case 4 -> {
+                return part.getInteractionWidth();
+            }
+            case 5 -> {
+                return part.getInteractionHeight();
+            }
+            default -> {
+                 return null;
+            }
+        }
     }
 
     @Override
@@ -77,65 +96,72 @@ public class ExprActivePartData extends SimplePropertyExpression<ActivePart, Obj
 
     @Override
     public void change(Event event, Object[] delta, Changer.ChangeMode mode){
-        ActivePart part = getExpr().getSingle(event);
-        assert delta != null;
-        Object obj = delta[0];
-        if (part == null){
-            return;
-        }
-
-        switch(mark){
-            case 1 -> {
-                if (mode == Changer.ChangeMode.RESET){
-                    part.setBlockDisplayBlock(Material.AIR.createBlockData());
-                    return;
-                }
-                if (obj instanceof BlockData bd){
-                    part.setBlockDisplayBlock(bd);
-                }
-                else if (obj instanceof ItemType it){
-                    part.setBlockDisplayBlock(it.getMaterial().createBlockData());
-                }
-            }
-            case 2 -> {
-                if (mode == Changer.ChangeMode.RESET){
-                    part.setItemDisplayItem(new ItemStack(Material.AIR));
-                }
-                else{
-                    if (obj instanceof ItemStack i){
-                        part.setItemDisplayItem(i);
+        if (delta == null) return;
+        for (ActivePart part : getExpr().getArray(event)){
+            if (part == null) continue;
+            Object obj = delta[0];
+//          if (part == null){
+//             return;
+//          }
+            switch(mark){
+                //Block Displays
+                case 1 -> {
+                    if (mode == Changer.ChangeMode.RESET){
+                        part.setBlockDisplayBlock(Material.AIR.createBlockData());
+                        return;
                     }
-                    else if (obj instanceof BlockData bd){
-                        part.setItemDisplayItem(new ItemStack(bd.getMaterial()));
+                    if (obj instanceof BlockData bd){
+                        part.setBlockDisplayBlock(bd);
+                    }
+                    else if (obj instanceof ItemType it){
+                        part.setBlockDisplayBlock(it.getMaterial().createBlockData());
+                    }
+                }
+                //Item Displays
+                case 2 -> {
+                    if (mode == Changer.ChangeMode.RESET){
+                        part.setItemDisplayItem(new ItemStack(Material.AIR));
                     }
                     else{
-                        part.setItemDisplayItem(new ItemStack(((ItemType) obj).getMaterial()));
+                        if (obj instanceof ItemStack i){
+                            part.setItemDisplayItem(i);
+                        }
+                        else if (obj instanceof BlockData bd){
+                            part.setItemDisplayItem(new ItemStack(bd.getMaterial()));
+                        }
+                        else{
+                            part.setItemDisplayItem(new ItemStack(((ItemType) obj).getMaterial()));
+                        }
                     }
                 }
-            }
-            case 3 -> {
-                if (mode == Changer.ChangeMode.RESET){
-                    part.setTextDisplayText(Component.empty());
+                //Text Displays
+                case 3 -> {
+                    if (mode == Changer.ChangeMode.RESET){
+                        part.setTextDisplayText(Component.empty());
+                    }
+                    else{
+                        String value = delta == null ? null : String.join("\n", Arrays.copyOf(delta, delta.length, String[].class));
+                        BaseComponent[] components = BungeeConverter.convert(ChatMessages.parseToArray(value));
+                        part.setTextDisplayText(BungeeComponentSerializer.get().deserialize(components));
+                    }
                 }
-                else{
-                    BaseComponent[] components = BungeeConverter.convert(ChatMessages.parseToArray((String) obj));
-                    part.setTextDisplayText(BungeeComponentSerializer.get().deserialize(components));
+                //Interaction Width
+                case 4 -> {
+                    if (mode == Changer.ChangeMode.RESET){
+                        part.setInteractionWidth(1);
+                    }
+                    else{
+                        part.setInteractionWidth(((Number) obj).floatValue());
+                    }
                 }
-            }
-            case 4 -> {
-                if (mode == Changer.ChangeMode.RESET){
-                    part.setInteractionWidth(1);
-                }
-                else{
-                    part.setInteractionWidth(((Number) obj).floatValue());
-                }
-            }
-            case 5 -> {
-                if (mode == Changer.ChangeMode.RESET){
-                    part.setInteractionHeight(1);
-                }
-                else{
-                    part.setInteractionHeight(((Number) obj).floatValue());
+                //Interaction Height
+                case 5 -> {
+                    if (mode == Changer.ChangeMode.RESET){
+                        part.setInteractionHeight(1);
+                    }
+                    else{
+                        part.setInteractionHeight(((Number) obj).floatValue());
+                    }
                 }
             }
         }
