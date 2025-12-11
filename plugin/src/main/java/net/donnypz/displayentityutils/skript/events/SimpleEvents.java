83bd84931a758376612bd5256e556a79108bbf95
Jay <76460079+PZDonny@users.noreply.github.com>
2025-12-11 01:54:00 -0600

    Fix PrePacketGroupCreateEvent incorrect syntax

--- plugin/src/main/java/net/donnypz/displayentityutils/skript/events/SimpleEvents.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/events/SimpleEvents.java
@@ -65,7 +65,7 @@ public class SimpleEvents extends SimpleEvent {
             EventValues.registerEventValue(PacketGroupSendEvent.class, Player[].class, e -> e.getPlayers().toArray(new Player[0]));
 
             //Pre Packet Group Create Event
-            Skript.registerEvent("Packet Group Created", SimpleEvents.class, PrePacketGroupCreateEvent.class, "packet [spawned] [display] group (destroy[ed]|remove[d]|hid(e|den))")
+            Skript.registerEvent("Pre Packet Group Created", SimpleEvents.class, PrePacketGroupCreateEvent.class, "pre packet [spawned] [display] group (created[ed]|spawn[ed])")
                     .description("Called before a packet-based group is created")
                     .since("3.3.6");
             EventValues.registerEventValue(PrePacketGroupCreateEvent.class, DisplayEntityGroup.class, PrePacketGroupCreateEvent::getGroup);
