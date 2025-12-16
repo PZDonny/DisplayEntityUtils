c3dc3348a7c44e6ee3dab12f53525205dfc01598
Jay <76460079+PZDonny@users.noreply.github.com>
2025-12-16 11:24:03 -0600

    Added `/deu text addline`

--- plugin/src/main/java/net/donnypz/displayentityutils/command/text/TextAddLineCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/text/TextAddLineCMD.java
@@ -16,16 +16,16 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-class TextSetCMD extends PartsSubCommand {
+class TextAddLineCMD extends PartsSubCommand {
 
-    public TextSetCMD(@NotNull DEUSubCommand parentSubCommand) {
-        super("set", parentSubCommand, Permission.TEXT_SET_TEXT, 3, 0);
+    public TextAddLineCMD(@NotNull DEUSubCommand parentSubCommand) {
+        super("addline", parentSubCommand, Permission.TEXT_SET_TEXT, 3, 0);
         setTabComplete(2, "<text>");
     }
 
     @Override
     protected void sendIncorrectUsage(@NotNull Player player) {
-        player.sendMessage(Component.text("Incorrect Usage! /deu text set <text>", NamedTextColor.RED));
+        player.sendMessage(Component.text("Incorrect Usage! /deu text addline <text>", NamedTextColor.RED));
     }
 
     @Override
@@ -37,21 +37,9 @@ class TextSetCMD extends PartsSubCommand {
     protected boolean executeSinglePartAction(@NotNull Player player, @Nullable ActiveGroup<?> group, @NotNull ActivePartSelection<?> selection, @NotNull ActivePart selectedPart, @NotNull String[] args) {
         Component currentText = selectedPart.getTextDisplayText();
         Key font = currentText.font();
-        Component comp = LegacyComponentSerializer.legacyAmpersand().deserialize(getTextResult(args));
+        Component comp = currentText.appendNewline().append(LegacyComponentSerializer.legacyAmpersand().deserialize(TextSetCMD.getTextResult(args)));
         selectedPart.setTextDisplayText(comp.font(font));
-        player.sendMessage(DisplayAPI.pluginPrefix.append(Component.text("Successfully set text on text display!", NamedTextColor.GREEN)));
-        player.sendMessage(Component.text("You can include \"\\n\" in your to create a new line.", NamedTextColor.GRAY));
+        player.sendMessage(DisplayAPI.pluginPrefix.append(Component.text("Successfully added line to text display!", NamedTextColor.GREEN)));
         return true;
-    }
-
-    static String getTextResult(String[] args){
-        StringBuilder builder = new StringBuilder();
-        for (int i = 2; i < args.length; i++){
-            builder.append(args[i]);
-            if (i+1 != args.length){
-                builder.append(" ");
-            }
-        }
-        return builder.toString().replace("\\n", "\n");
     }
 }
--- plugin/src/main/java/net/donnypz/displayentityutils/command/text/TextCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/text/TextCMD.java
@@ -11,6 +11,7 @@ public final class TextCMD extends ConsoleUsableSubCommand {
         super(Permission.HELP, new TextHelpCMD());
         new TextEditCMD(this);
         new TextSetCMD(this);
+        new TextAddLineCMD(this);
         new TextFontCMD(this);
         new TextShadowCMD(this);
         new TextSeeThroughCMD(this);
@@ -38,16 +39,21 @@ public final class TextCMD extends ConsoleUsableSubCommand {
 
     static void help(CommandSender sender, int page){
         sender.sendMessage(DisplayAPI.pluginPrefixLong);
-        CMDUtils.sendCMD(sender, "/deu text help", "Get help for text displays");
-        CMDUtils.sendCMD(sender, "/deu text edit [-&]", "Open a dialog menu to edit all text display properties. Add the \"-&\" parameter to format the text with \"&\"");
-        CMDUtils.sendCMD(sender, "/deu text set <text>", "Set this text for your selected text display");
-        CMDUtils.sendCMD(sender, "/deu text font <default | alt | uniform | illageralt> [-all]", "Set the text font for your selected text display");
-        CMDUtils.sendCMD(sender, "/deu text shadow [-all <on | off>]", "Toggle shadows visibility in your selected text display");
-        CMDUtils.sendCMD(sender, "/deu text seethrough [-all <on | off>]", "Toggle see through setting of your selected text display");
-        CMDUtils.sendCMD(sender, "/deu text align <left | right | center> [-all]", "Set your selected text display's text alignment");
-        CMDUtils.sendCMD(sender, "/deu text linewidth <width> [-all]", "Set the line width of your selected text display");
-        CMDUtils.sendCMD(sender, "/deu text background <color | hex-code> <0-1> [-all]", "Set the background color of a text display, and the opacity");
-        CMDUtils.sendCMD(sender, "/deu text opacity <0-1> [-all]", "Set the text opacity for your selected text display");
-        sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray><bold>--------------------------"));
+        if (page == 1){
+            CMDUtils.sendCMD(sender, "/deu text help", "Get help for text displays");
+            CMDUtils.sendCMD(sender, "/deu text edit [-&]", "Open a dialog menu to edit all text display properties. Add the \"-&\" parameter to format the text with \"&\"");
+            CMDUtils.sendCMD(sender, "/deu text set <text>", "Set this text for your selected text display");
+            CMDUtils.sendCMD(sender, "/deu text addline <text>", "Add a line of text to your selected text display");
+            CMDUtils.sendCMD(sender, "/deu text font <default | alt | uniform | illageralt> [-all]", "Set the text font for your selected text display");
+            CMDUtils.sendCMD(sender, "/deu text shadow [-all <on | off>]", "Toggle shadows visibility in your selected text display");
+            CMDUtils.sendCMD(sender, "/deu text seethrough [-all <on | off>]", "Toggle see through setting of your selected text display");
+        }
+        else{
+            CMDUtils.sendCMD(sender, "/deu text align <left | right | center> [-all]", "Set your selected text display's text alignment");
+            CMDUtils.sendCMD(sender, "/deu text linewidth <width> [-all]", "Set the line width of your selected text display");
+            CMDUtils.sendCMD(sender, "/deu text background <color | hex-code> <0-1> [-all]", "Set the background color of a text display, and the opacity");
+            CMDUtils.sendCMD(sender, "/deu text opacity <0-1> [-all]", "Set the text opacity for your selected text display");
+        }
+        sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray><bold>----------</bold><yellow>Page "+page+"<gray><bold>----------"));
     }
 }
