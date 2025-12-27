ced9db8f532548fea9d8955c48e82d2cf623f97a
Jay <76460079+PZDonny@users.noreply.github.com>
2025-12-27 00:33:08 -0600

    add "/deu mannequin unname"

--- plugin/src/main/java/net/donnypz/displayentityutils/command/mannequin/MannequinCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/mannequin/MannequinCMD.java
@@ -12,6 +12,7 @@ public class MannequinCMD extends ConsoleUsableSubCommand {
     public MannequinCMD() {
         super(Permission.HELP, new MannequinHelpCMD());
         new MannequinNameCMD(this);
+        new MannequinUnnameCMD(this);
         new MannequinBelowNameCMD(this);
         new MannequinToggleNameVisibilityCMD(this);
         new MannequinSkinCMD(this);
@@ -51,13 +52,14 @@ public class MannequinCMD extends ConsoleUsableSubCommand {
             sender.sendMessage(Component.text("| Commands with \"-all\" will apply the command to all mannequins within a part selection", NamedTextColor.GOLD));
             CMDUtils.sendCMD(sender, "/deu mannequin help", "Get help for mannequins");
             CMDUtils.sendCMD(sender, "/deu mannequin name <name>", "Set your selected mannequin's name");
+            CMDUtils.sendCMD(sender, "/deu mannequin unname", "Remove your selected mannequin's name");
             CMDUtils.sendCMD(sender, "/deu mannequin belowname <text>", "Set the text below your selected mannequin's name");
             CMDUtils.sendCMD(sender, "/deu mannequin togglenamevisibility [-all <on | off>]", "Toggle the gravity of an mannequin");
             CMDUtils.sendCMD(sender, "/deu mannequin skin <player-name> [-all]", "Set your selected mannequin's skin");
             CMDUtils.sendCMD(sender, "/deu mannequin togglegravity [-all <on | off>]", "Toggle the gravity of an mannequin");
-            CMDUtils.sendCMD(sender, "/deu mannequin toggleimmovable [-all <on | off>]", "Toggle the immovability of an mannequin");
         }
         else{
+            CMDUtils.sendCMD(sender, "/deu mannequin toggleimmovable [-all <on | off>]", "Toggle the immovability of an mannequin");
             CMDUtils.sendCMD(sender, "/deu mannequin equipment ", "Open a GUI to edit your selected mannequin's equipment.");
             CMDUtils.sendCMD(sender, "/deu mannequin mainhand <left | right>", "Set your selected mannequin's main hand");
             CMDUtils.sendCMD(sender, "/deu mannequin pivot <angle> [-all]");
