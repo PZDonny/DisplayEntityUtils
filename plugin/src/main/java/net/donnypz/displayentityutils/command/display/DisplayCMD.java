cf10f8140ca918e805f209721b0d7dd5a328a13d
Jay <76460079+PZDonny@users.noreply.github.com>
2026-01-01 04:20:58 -0600

    Add "/deu display resettranslation"

--- plugin/src/main/java/net/donnypz/displayentityutils/command/display/DisplayCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/display/DisplayCMD.java
@@ -2,10 +2,12 @@ package net.donnypz.displayentityutils.command.display;
 
 import net.donnypz.displayentityutils.DisplayAPI;
 import net.donnypz.displayentityutils.command.*;
+import net.donnypz.displayentityutils.utils.DisplayEntities.ActivePart;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public final class DisplayCMD extends ConsoleUsableSubCommand {
 
@@ -17,6 +19,7 @@ public final class DisplayCMD extends ConsoleUsableSubCommand {
         new DisplayViewRangeCMD(this);
         new DisplayBillboardCMD(this);
         new DisplayTranslateCMD(this);
+        new DisplayResetTranslationCMD(this);
         new DisplayScaleCMD(this);
         new DisplaySetBlockCMD(this);
     }
@@ -41,15 +44,26 @@ public final class DisplayCMD extends ConsoleUsableSubCommand {
         sender.sendMessage(Component.empty());
         sender.sendMessage(DisplayAPI.pluginPrefixLong);
         sender.sendMessage(Component.text("| Commands with \"-all\" will apply the command to all display entities in your selection", NamedTextColor.GOLD));
-
+        if (page == 1){
             CMDUtils.sendCMD(sender, "/deu display glowcolor <color | hex-code> [-all]", "Set your selected display part's glow color");
             CMDUtils.sendCMD(sender, "/deu display brightness <block> <sky> [-all]", "Set your selected display part's brightness. Enter values between 0-15. -1 resets");
             CMDUtils.sendCMD(sender, "/deu display viewrange <view-range-multiplier> [-all]", "Set the view range multiplier for your selected display part");
             CMDUtils.sendCMD(sender, "/deu display billboard <fixed | vertical | horizontal | center> [-all]", "Set the billboard of your selected display part");
             CMDUtils.sendCMD(sender, "/deu display translate <direction> <distance> <tick-duration> [-all]", "Translate your selected display part");
+            CMDUtils.sendCMD(sender, "/deu display resettranslation [-all]", "Reset the translation of your selected display part");
             CMDUtils.sendCMD(sender, "/deu display scale <x | y | z | -all> <scale> [-all]", "Change the scale of your selected display part, by axis");
+        }
+        else{
             CMDUtils.sendCMD(sender, "/deu display setblock <\"-held\" | \"-target\" | block-id> [-all]", "Change the block of a block display part");
-        sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray><bold>--------------------------"));
-        //sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray><bold>----------</bold><yellow>Page "+page+"<gray><bold>----------"));
+        }
+        sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray><bold>----------</bold><yellow>Page "+page+"<gray><bold>----------"));
+    }
+
+    static boolean isDisplay(Player player, ActivePart part){
+        if (!part.isDisplay()){
+            player.sendMessage(DisplayAPI.pluginPrefix.append(Component.text("You can only do this display entity parts!", NamedTextColor.RED)));
+            return false;
+        }
+        return true;
     }
 }
