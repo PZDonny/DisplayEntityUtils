23c3b54a80c660dcb6e4d9dc4a73bcaa4d7153a6
Jay <76460079+PZDonny@users.noreply.github.com>
2026-02-08 15:22:57 -0600

    Add "Player's Selected Active Part" syntax

---
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/expressions/ExprPlayerSelectedPart.java
@@ -0,0 +1,50 @@
+package net.donnypz.displayentityutils.skript.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import net.donnypz.displayentityutils.managers.DisplayGroupManager;
+import net.donnypz.displayentityutils.utils.DisplayEntities.ActivePart;
+import net.donnypz.displayentityutils.utils.DisplayEntities.ActivePartSelection;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Player's Selected Active Part")
+@Description("Get the selected active part of a player")
+@Examples({
+        "set {_activepart} to player's selected part"
+})
+@Since("3.4.3")
+public class ExprPlayerSelectedPart extends SimplePropertyExpression<Player, ActivePart> {
+    static {
+        register(ExprPlayerSelectedPart.class, ActivePart.class, "selected [active] [display] [entity] part", "players");
+    }
+
+    @Override
+    public Class<? extends ActivePart> getReturnType() {
+        return ActivePart.class;
+    }
+
+    @Override
+    @Nullable
+    public ActivePart convert(Player player) {;
+        if (player != null){
+            ActivePartSelection<?> sel = DisplayGroupManager.getPartSelection(player);
+            if (sel == null) return null;
+            return sel.getSelectedPart();
+        }
+        return null;
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "selected active part";
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+}
