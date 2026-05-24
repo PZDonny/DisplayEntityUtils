c242d999a6bd5e03a7fb58993e7e79ef90bf4165
Jay <76460079+PZDonny@users.noreply.github.com>
2026-05-23 21:44:00 -0500

    Remove duplicate listings in list cmds, add message showing that items are clickable

--- plugin/src/main/java/net/donnypz/displayentityutils/command/ListCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/ListCMD.java
@@ -10,6 +10,7 @@ import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -17,11 +18,12 @@ public class ListCMD extends ConsoleUsableSubCommand {
     Component incorrectUsageMessage;
     int minLength, storageIndex, pageNumberIndex;
     boolean listsGroups;
+
     public ListCMD(Component incorrectUsageMessage, int minLength, boolean listsGroups) {
         super(listsGroups ? Permission.LIST_GROUPS : Permission.LIST_ANIMATIONS);
         this.incorrectUsageMessage = incorrectUsageMessage;
         this.minLength = minLength;
-        this.storageIndex = minLength-1;
+        this.storageIndex = minLength - 1;
         this.pageNumberIndex = minLength;
         this.listsGroups = listsGroups;
         setTabComplete(storageIndex, TabSuggestion.STORAGES);
@@ -30,74 +32,69 @@ public class ListCMD extends ConsoleUsableSubCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (args.length < minLength){
+        if (args.length < minLength) {
             sender.sendMessage(incorrectUsageMessage);
             return;
         }
-        list(sender,storageIndex, pageNumberIndex, args);
+        list(sender, storageIndex, pageNumberIndex, args);
     }
 
-    public void list(CommandSender sender, int storageIndex, int pageNumberIndex, String[] args){
+    public void list(CommandSender sender, int storageIndex, int pageNumberIndex, String[] args) {
         DisplayAPI.getScheduler().runAsync(() -> {
-            LoadMethod method;
-            try{
-                method = LoadMethod.valueOf(args[storageIndex].toUpperCase());
-            }
-            catch(IllegalArgumentException e){
+            LoadMethod loadMethod;
+            try {
+                loadMethod = LoadMethod.valueOf(args[storageIndex].toUpperCase());
+            } catch (IllegalArgumentException e) {
                 DisplayEntityPluginCommand.invalidStorage(sender);
                 return;
             }
 
             List<String> tags = listsGroups ?
-                    DisplayGroupManager.getSavedDisplayEntityGroups(method)
+                    DisplayGroupManager.getSavedDisplayEntityGroups(loadMethod)
                     :
-                    DisplayAnimationManager.getSavedDisplayAnimations(method);
+                    DisplayAnimationManager.getSavedDisplayAnimations(loadMethod);
 
             sender.sendMessage(DisplayAPI.pluginPrefixLong);
-            sender.sendMessage(MiniMessage.miniMessage().deserialize("Storage Location: <yellow>"+method.getDisplayName()));
-            if (tags.isEmpty()){
+            sender.sendMessage(MiniMessage.miniMessage().deserialize("Storage Location: <yellow>" + loadMethod.getDisplayName()));
+            if (tags.isEmpty()) {
                 sender.sendMessage(Component.text("That storage location is empty!", NamedTextColor.RED));
                 return;
             }
 
-            int pageNumber = 1;
-            if (args.length > minLength){
-                try{
-                    pageNumber = Math.max(1, Integer.parseInt(args[pageNumberIndex]));
-                }
-                catch(NumberFormatException ignored){}
+            if (sender instanceof Player) {
+                sender.sendMessage(Component.text("Click to a listed item to " + (listsGroups ? "spawn" : "select") + " it", NamedTextColor.GRAY));
             }
 
-            int end = pageNumber*7;
-            int start = end-7;
-            sender.sendMessage(Component.text("Page Number: "+pageNumber, NamedTextColor.AQUA));
-            for (int i = start; i <= end; i++){
-                if (i >= tags.size()){
+            int pageNumber = 1;
+            if (args.length > minLength) {
+                try {
+                    pageNumber = Math.max(1, Integer.parseInt(args[pageNumberIndex]));
+                } catch (NumberFormatException ignored) {
+                }
+            }
+
+            int end = pageNumber * 6;
+            int start = end - 6;
+
+            for (int i = start; i < end; i++) {
+                if (i >= tags.size()) {
                     break;
                 }
-                Component message;
                 String tag = tags.get(i);
-                if (listsGroups){
-                    message = spawnGroup(tag, method);
-                }
-                else{
-                    message = selectAnimation(tag, method);
-                }
-                sender.sendMessage(message);
+                sender.sendMessage(getMessage(tag, loadMethod));
             }
-            sender.sendMessage("------------------------");
+            sender.sendMessage(Component.text("----------" + "Page " + pageNumber + "----------", NamedTextColor.GRAY));
         });
     }
 
-    private static Component spawnGroup(String tag, LoadMethod loadMethod){
-        return MiniMessage.miniMessage().deserialize("- <yellow>"+tag)
-                .hoverEvent(HoverEvent.showText(Component.text("Click to spawn", NamedTextColor.AQUA)))
-                .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/deu group spawn "+tag+" "+loadMethod.name()));
-    }
 
-    private static Component selectAnimation(String tag, LoadMethod loadMethod){
-        return MiniMessage.miniMessage().deserialize("- <yellow>"+tag)
-                .hoverEvent(HoverEvent.showText(Component.text("Click to select", NamedTextColor.AQUA)))
-                .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/deu anim select "+tag+" "+loadMethod.name()));
+    private Component getMessage(String tag, LoadMethod loadMethod) {
+        return MiniMessage.miniMessage().deserialize("- <yellow>" + tag)
+                .hoverEvent(HoverEvent.showText(
+                        Component.text(
+                                (listsGroups ? "Click to spawn" : "Click to select"),
+                                NamedTextColor.GREEN))
+                ).clickEvent(ClickEvent.clickEvent(ClickEvent.Action.SUGGEST_COMMAND,
+                        "/deu " + (listsGroups ? "group spawn " : "anim select ") + tag + " " + loadMethod.name()));
     }
 }
