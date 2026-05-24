f66a02a815eaa4a58f9006fdb0676f2455987104
Jay <76460079+PZDonny@users.noreply.github.com>
2026-05-23 21:22:08 -0500

    Added "/deu mannequin spawn" command

--- plugin/src/main/java/net/donnypz/displayentityutils/command/mannequin/MannequinCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/mannequin/MannequinCMD.java
@@ -11,6 +11,7 @@ import org.bukkit.command.CommandSender;
 public class MannequinCMD extends ConsoleUsableSubCommand {
     public MannequinCMD() {
         super(Permission.HELP, new MannequinHelpCMD());
+        new MannequinSpawnCMD(this);
         new MannequinNameCMD(this);
         new MannequinUnnameCMD(this);
         new MannequinBelowNameCMD(this);
@@ -51,14 +52,15 @@ public class MannequinCMD extends ConsoleUsableSubCommand {
         if (page == 1){
             sender.sendMessage(Component.text("| Commands with \"-all\" will apply the command to all mannequins within a part selection", NamedTextColor.GOLD));
             CMDUtils.sendCMD(sender, "/deu mannequin help", "Get help for mannequins");
+            CMDUtils.sendCMD(sender, "/deu mannequin spawn [-g]", "Spawn a mannequin at your location.\nUse \"-g\" to add it to your selected group");
             CMDUtils.sendCMD(sender, "/deu mannequin name <name>", "Set your selected mannequin's name");
             CMDUtils.sendCMD(sender, "/deu mannequin unname [-all]", "Remove your selected mannequin's name");
             CMDUtils.sendCMD(sender, "/deu mannequin belowname <text>", "Set the text below your selected mannequin's name");
             CMDUtils.sendCMD(sender, "/deu mannequin togglenamevisibility [-all <on | off>]", "Toggle the gravity of an mannequin");
             CMDUtils.sendCMD(sender, "/deu mannequin skin <player-name> [-all]", "Set your selected mannequin's skin");
-            CMDUtils.sendCMD(sender, "/deu mannequin togglegravity [-all <on | off>]", "Toggle the gravity of an mannequin");
         }
         else{
+            CMDUtils.sendCMD(sender, "/deu mannequin togglegravity [-all <on | off>]", "Toggle the gravity of an mannequin");
             CMDUtils.sendCMD(sender, "/deu mannequin toggleimmovable [-all <on | off>]", "Toggle the immovability of an mannequin");
             CMDUtils.sendCMD(sender, "/deu mannequin equipment ", "Open a GUI to edit your selected mannequin's equipment.");
             CMDUtils.sendCMD(sender, "/deu mannequin mainhand <left | right>", "Set your selected mannequin's main hand");
---
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/mannequin/MannequinSpawnCMD.java
@@ -0,0 +1,61 @@
+package net.donnypz.displayentityutils.command.mannequin;
+
+import io.papermc.paper.datacomponent.item.ResolvableProfile;
+import net.donnypz.displayentityutils.DisplayAPI;
+import net.donnypz.displayentityutils.command.DEUSubCommand;
+import net.donnypz.displayentityutils.command.Permission;
+import net.donnypz.displayentityutils.command.PlayerSubCommand;
+import net.donnypz.displayentityutils.managers.DEUUser;
+import net.donnypz.displayentityutils.utils.DisplayEntities.*;
+import net.donnypz.displayentityutils.utils.DisplayUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Location;
+import org.bukkit.entity.Mannequin;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+class MannequinSpawnCMD extends PlayerSubCommand {
+    MannequinSpawnCMD(@NotNull DEUSubCommand parentSubCommand) {
+        super("spawn", parentSubCommand, Permission.MANNEQUIN_SPAWN);
+        setTabComplete(2, List.of("-g"));
+    }
+
+    protected void sendIncorrectUsage(@NotNull Player player) {
+        player.sendMessage(DisplayAPI.pluginPrefix.append(Component.text("Incorrect ALL usage! /deu mannequin spawn [-g]", NamedTextColor.RED)));
+    }
+
+    @Override
+    public void execute(Player player, String[] args) {
+        Location location = player.getLocation();
+        Mannequin mannequin = location.getWorld().spawn(location, Mannequin.class, m -> {
+            m.setProfile(ResolvableProfile.resolvableProfile(player.getPlayerProfile()));
+            m.customName(Component.text("New Mannequin"));
+            m.setCustomNameVisible(true);
+        });
+        DisplayUtils.prepareMannequin(mannequin);
+
+        player.sendMessage(DisplayAPI.pluginPrefix
+                .append(MiniMessage.miniMessage().deserialize("<green>A new mannequin has been spawned at your location!")));
+
+
+        if (args.length >= 3 && args[2].equalsIgnoreCase("-g")){
+            ActiveGroup<?> group = DEUUser.getOrCreateUser(player).getSelectedGroup();
+            if (group == null) {
+                player.sendMessage(Component.text("- You must have a group selected to add the mannequin to a group", NamedTextColor.YELLOW));
+                return;
+            }
+
+            ActivePart part = group.addEntity(mannequin);
+            if (part == null){
+                player.sendMessage(Component.text("- Failed to add the mannequin to your selected group", NamedTextColor.YELLOW));
+                return;
+            }
+
+            player.sendMessage(Component.text("- The mannequin has been added to your selected group", NamedTextColor.GRAY));
+        }
+    }
+}
