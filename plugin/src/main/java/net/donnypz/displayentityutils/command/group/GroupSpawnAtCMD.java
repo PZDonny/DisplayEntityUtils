00591318a8daa1ec25d2340a68ebd747630fd19c
Jay <76460079+PZDonny@users.noreply.github.com>
2026-06-18 20:38:05 -0500

    Use coordinate tab completes, utilize flags & options

--- plugin/src/main/java/net/donnypz/displayentityutils/command/group/GroupSpawnAtCMD.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/command/group/GroupSpawnAtCMD.java
@@ -4,7 +4,6 @@ import net.donnypz.displayentityutils.DisplayAPI;
 import net.donnypz.displayentityutils.command.ConsoleUsableSubCommand;
 import net.donnypz.displayentityutils.command.DEUSubCommand;
 import net.donnypz.displayentityutils.command.Permission;
-import net.donnypz.displayentityutils.command.PlayerSubCommand;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
@@ -14,28 +13,23 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 public class GroupSpawnAtCMD extends ConsoleUsableSubCommand {
     GroupSpawnAtCMD(@NotNull DEUSubCommand parentSubCommand) {
         super("spawnat", parentSubCommand, Permission.GROUP_SPAWN);
-        setTabComplete(2, "<x | ~>");
-        setTabComplete(3, "<y | ~>");
-        setTabComplete(4, "<z | ~>");
+        setTabComplete(2, TabSuggestion.X_COORDINATE);
+        setTabComplete(3, TabSuggestion.Y_COORDINATE);
+        setTabComplete(4, TabSuggestion.Z_COORDINATE);
         setTabComplete(5, "<group-tag>");
         setTabComplete(6, TabSuggestion.STORAGES);
-        setTabComplete(7, List.of("-world <world_name>", "-packet", "-force"));
-        setTabComplete(8, List.of("-world <world_name>", "-packet", "-force"));
-        setTabComplete(9, List.of("-world <world_name>", "-packet", "-force"));
-        setTabComplete(10, List.of("-world <world_name>", "-packet", "-force"));
+        addFlag("-packet");
+        addFlag("-force");
+        addOption("-world", "<world_name>");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (args.length < 7) {
-            sender.sendMessage(Component.text("Incorrect Usage! /deu group spawnat <x> <y> <z> <group-tag> <storage> [-world <world_name>] [-packet] [-force]", NamedTextColor.RED));
-            return;
-        }
+        if (!hasMinimumArguments(sender, args)) return;
+
         try{
             double x = getCoordinate(sender, args[2], 'x');
             double y = getCoordinate(sender, args[3], 'y');
@@ -43,9 +37,10 @@ public class GroupSpawnAtCMD extends ConsoleUsableSubCommand {
             String tag = args[5];
             String storage = args[6];
 
-            OptionalArgs optionalArgs = new OptionalArgs(sender, args);
+            OptionalArguments optionalArgs = getOptionalArguments(sender, args);
             World w;
-            if (optionalArgs.worldName == null){
+            String worldName = optionalArgs.getOption("-world");
+            if (worldName.isEmpty()){
                 if (sender instanceof Player player){
                     w = player.getWorld();
                 }
@@ -56,7 +51,7 @@ public class GroupSpawnAtCMD extends ConsoleUsableSubCommand {
                 }
             }
             else{
-                w = Bukkit.getWorld(optionalArgs.worldName);
+                w = Bukkit.getWorld(worldName);
             }
 
             if (w == null){
@@ -65,7 +60,7 @@ public class GroupSpawnAtCMD extends ConsoleUsableSubCommand {
             }
 
             Location spawnLocation = new Location(w, x, y, z);
-            GroupSpawnCMD.spawnGroup(sender, spawnLocation, tag, storage, optionalArgs.packet);
+            GroupSpawnCMD.spawnGroup(sender, spawnLocation, tag, storage, optionalArgs.hasFlag("-packet"));
         }catch(NumberFormatException e){
             sender.sendMessage(DisplayAPI.pluginPrefix.append(Component.text("Enter a number for each coordinate!", NamedTextColor.RED)));
         }
@@ -89,50 +84,6 @@ public class GroupSpawnAtCMD extends ConsoleUsableSubCommand {
         }
         else{
             return Double.parseDouble(userInput);
-        }
-    }
-
-    class OptionalArgs{
-        String worldName = null;
-        boolean packet = false;
-        boolean force = false;
-
-        OptionalArgs(CommandSender sender, String[] args){
-
-
-            for (int i = 7; i < args.length; i++) {
-                String arg = args[i];
-
-                switch (arg.toLowerCase()) {
-                    case "-packet" -> packet = true;
-                    case "-force" -> force = true;
-
-                    case "-world" -> {
-                        if (worldName != null) {
-                            sender.sendMessage(Component.text(
-                                    "-world was specified multiple times.",
-                                    NamedTextColor.RED));
-                            return;
-                        }
-
-                        if (i + 1 >= args.length) {
-                            sender.sendMessage(Component.text(
-                                    "You must specify a world after -world.",
-                                    NamedTextColor.RED));
-                            return;
-                        }
-
-                        worldName = args[++i];
-                    }
-
-                    default -> {
-                        sender.sendMessage(Component.text(
-                                "Unknown option: " + arg,
-                                NamedTextColor.RED));
-                        return;
-                    }
-                }
-            }
         }
     }
 }
