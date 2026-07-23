f29cc9ab71a317763ada782440127cb85288d79a
Jay <76460079+PZDonny@users.noreply.github.com>
2026-07-23 12:11:50 -0500

    Add rotating around a location, and Active and Display types to rotate syntax

--- plugin/src/main/java/net/donnypz/displayentityutils/skript/active/effects/EffActiveRotate.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/active/effects/EffActiveRotate.java
@@ -8,20 +8,23 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
-import net.donnypz.displayentityutils.utils.DisplayEntities.ActiveGroup;
-import net.donnypz.displayentityutils.utils.DisplayEntities.ActivePart;
+import net.donnypz.displayentityutils.utils.DisplayEntities.Active;
+import net.donnypz.displayentityutils.utils.DisplayUtils;
 import org.bukkit.Location;
+import org.bukkit.entity.Display;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Quaternionf;
 import org.skriptlang.skript.registration.SyntaxInfo;
 import org.skriptlang.skript.registration.SyntaxRegistry;
 
-@Name("Rotate Active Group/Part")
-@Description("Update the axis rotation of an active group/part, in degrees. \nThis only applies to display entity parts and display entities in groups")
+@Name("Rotate")
+@Description("Update the axis rotation of an active group/part or display entity, in degrees. " +
+        "\nIn groups, this only applies to display entity parts and display entities.")
 @Examples({
         "deu rotate y of {_activegroup} by 45 degrees",
-        "deu rotate x of {_activegroup} by 30 degrees around {_pivotlocation}",
+        "deu rotate world y axis of {_partfilter} by 30 degrees around {_pivotlocation}",
+        "deu rotate z axis of {_display} by 90 degrees around {_pivotlocation}",
         "",
         "#Before 3.6.0",
         "deu rotate y of {_activegroup} by 45",
@@ -29,7 +32,7 @@ import org.skriptlang.skript.registration.SyntaxRegistry;
         "deu rotate z of {_activepart} by 20",
         "deu rotate world y of {_activepart} by 90"
         })
-@Since("3.4.3, 3.6.0 (Pivot)")
+@Since("3.4.3, 3.6.0 (Rotate around, more types)")
 public class EffActiveRotate extends Effect {
     Expression<?> object;
     Expression<Number> rotation;
@@ -40,7 +43,7 @@ public class EffActiveRotate extends Effect {
     public static void register(SyntaxRegistry registry){
         registry.register(SyntaxRegistry.EFFECT,
                 SyntaxInfo.builder(EffActiveRotate.class)
-                        .addPattern("deu rotate  (:x|:y|:z) of %activegroup/activepart% by %number% [degrees] [a:around %-location%]")
+                        .addPattern("deu rotate [:world] (:x|:y|:z) [axis] of %activegroups/activeparts/partfilters/displays% by %number% [degrees] [a:[pivot(ed|ing)] around %-location%]")
                         .supplier(EffActiveRotate::new)
                         .build()
         );
@@ -62,39 +65,46 @@ public class EffActiveRotate extends Effect {
         return true;
     }
 
+    //TODO: add functionality for multiple entries, add functionality for displays
     @Override
     protected void execute(Event event) {
-        Object obj = object.getSingle(event);
-        if (obj == null) return;
+        Object[] objects = object.getArray(event);
+        if (objects == null) return;
 
         Number rot = rotation.getSingle(event);
         if (rot == null) return;
 
         Location pivotLoc = pivotLocation == null ? null : pivotLocation.getSingle(event);
         float rotRad = (float) Math.toRadians(rot.doubleValue());
-        Quaternionf q = new Quaternionf();
+        Quaternionf rotation = new Quaternionf();
+
         if (axis == 'x'){
-            q.rotateX(rotRad);
+            rotation.rotateX(rotRad);
         }
         else if (axis == 'y'){
-            q.rotateY(rotRad);
+            rotation.rotateY(rotRad);
         }
         else if (axis == 'z') {
-            q.rotateZ(rotRad);
+            rotation.rotateZ(rotRad);
         }
 
-        if (obj instanceof ActiveGroup<?> gr){
-            if (pivotLoc == null) gr.rotate(q, world);
-            else gr.rotateAround(q, pivotLoc, world);
-        }
-        else if (obj instanceof ActivePart part){
-            if (pivotLoc == null) part.rotate(q, world);
-            else part.rotateAround(q, pivotLoc, world);
+        for (Object o : objects){
+            if (o instanceof Active a){
+                if (pivotLoc == null) a.rotate(rotation, world);
+                else a.rotateAround(rotation, pivotLoc, world);
+            }
+            else if (o instanceof Display display){
+                if (pivotLoc == null) DisplayUtils.rotate(display, rotation, world);
+                else DisplayUtils.rotateAround(display, rotation, pivotLoc, world);
+            }
         }
     }
 
     @Override
     public String toString(@Nullable Event event, boolean debug) {
-        return "rotate: "+object.toString(event, debug)+"'s "+axis+" axis";
+        return String.format("rotate %s on %s %s axis",
+                object.toString(event, debug),
+                axis,
+                world ? "world" : "");
     }
 }
