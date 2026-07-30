ec71bcc5c34aaedaed9b54e77b655a5eec4f9b01
Jay <76460079+PZDonny@users.noreply.github.com>
2026-07-30 04:06:19 -0500

    cleanup syntax

--- plugin/src/main/java/net/donnypz/displayentityutils/skript/parts/conditions/CondTextDisplayIsShadowed.java
+++ plugin/src/main/java/net/donnypz/displayentityutils/skript/parts/conditions/CondTextDisplayIsShadowed.java
@@ -27,7 +27,7 @@ public class CondTextDisplayIsShadowed extends Condition {
     public static void register(SyntaxRegistry registry){
         registry.register(SyntaxRegistry.CONDITION,
                 SyntaxInfo.builder(CondTextDisplayIsShadowed.class)
-                        .addPattern("%activepart/display% (1¦(is|has)|2¦is(n't| not)) deu [text] [drop] shadow[ed|s]")
+                        .addPattern("%activepart/display% (1¦(is|has)|2¦(has no|does(n't| not) have)) deu [text] [drop] shadow[ed|s]")
                         .supplier(CondTextDisplayIsShadowed::new)
                         .build()
         );
