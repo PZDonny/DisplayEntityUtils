f26766353ce50ddc4e4ad83053a314187284939f
Jay <76460079+PZDonny@users.noreply.github.com>
2026-08-02 03:06:48 -0500

    Add #rotate and #set to PivotAxis, to rotate and set angle on a Quaternion

--- api/src/main/java/net/donnypz/displayentityutils/utils/PivotAxis.java
+++ api/src/main/java/net/donnypz/displayentityutils/utils/PivotAxis.java
@@ -1,7 +1,46 @@
 package net.donnypz.displayentityutils.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
 public enum PivotAxis {
     X,
     Y,
     Z;
+
+    /**
+     * Rotate a quaternion on this {@link PivotAxis}'s axis
+     * @param quaternionf the quaternion
+     * @param angleInDegrees the angle in degrees
+     * @return a new {@link Quaternionf} with the rotation applied
+     */
+    public @NotNull Quaternionf rotate(@NotNull Quaternionf quaternionf, float angleInDegrees){
+        Quaternionf newRot = new Quaternionf(quaternionf);
+        float angleDeg = (float) Math.toRadians(angleInDegrees);
+        switch(this){
+            case X -> newRot.rotateX(angleDeg);
+            case Y -> newRot.rotateY(angleDeg);
+            case Z -> newRot.rotateZ(angleDeg);
+        }
+        return newRot;
+    }
+
+    /**
+     * Set a quaternion's axis to a given angle
+     * @param quaternionf the quaternion
+     * @param angleInDegrees the angle in degrees
+     * @return a new {@link Quaternionf} with the axis representative of this set
+     */
+    public @NotNull Quaternionf set(@NotNull Quaternionf quaternionf, float angleInDegrees){
+        Vector3f euler = quaternionf.getEulerAnglesXYZ(new Vector3f());
+        float angleRad = (float) Math.toRadians(angleInDegrees);
+
+        switch (this) {
+            case X -> euler.x = angleRad;
+            case Y -> euler.y = angleRad;
+            case Z -> euler.z = angleRad;
+        }
+        return new Quaternionf().rotateXYZ(euler.x, euler.y, euler.z);
+    }
 }
